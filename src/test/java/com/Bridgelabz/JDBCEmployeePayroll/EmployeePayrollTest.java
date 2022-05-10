c6243899362149b5d2d125c1abf7d8a589db9d8b
unknown <priyanka.rahate23@gmail.com>
2022-05-10 12:40:46 +0530

    Creating PayrollService DataBase and Connecting the java Program to Database

---
+++ src/test/java/com/Bridgelabz/JDBCEmployeePayroll/EmployeePayrollTest.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz.JDBCEmployeePayroll;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class EmployeePayrollTest {
+	
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
